794d2b3d916e4cb2f45854932ffbac1602cbf30b
Nick L. <lesseosnick@gmail.com>
2023-12-13 13:20:07 -0800

    Updated: window size scale

--- scenes/objects/GameOver.gd
+++ scenes/objects/GameOver.gd
@@ -6,8 +6,11 @@ func _ready():
 	pass
 
 
-func _on_restart_button_pressed():
-	restart.emit()
+
 
 func _on_main_menu_pressed():
 	get_tree().change_scene_to_file("res://scenes/ui/main_menu.tscn")
+
+
+func _on_restart_button_pressed():
+	restart.emit()
